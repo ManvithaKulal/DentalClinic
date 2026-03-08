2c85f5ae163158f0a501eb2631530c1139760519
PRAJNESH111 <prajneshpajju094@gmail.com>
2026-03-08 14:29:42 +0530

    Latest commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const { MongoStore } = require("connect-mongo");
 const passport = require("./config/passport");
 const path = require("path");
 
